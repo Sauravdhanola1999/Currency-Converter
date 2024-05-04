f46a8d8d35a521b190fa8092b73575ddd7b09e17
Saurav Singh Dhanola <125140898+Sauravdhanola1999@users.noreply.github.com>
2024-05-04 14:05:09 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import CurrencyConverter from "./Components/CurrencyConverter"
+
+function App() {
+
+  return (
+   <div className="min-h-screen bg-gray-100 flex flex-col 
+   items-center justify-center">
+  
+    <CurrencyConverter />
+
+   </div>
+  )
+}
+
+export default App
